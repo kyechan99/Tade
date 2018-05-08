36563e5597236a2691eaf4ab01b42e97b55020af
kyechan99 <kyechan99@naver.com>
2018-05-08 16:24:11 +0900

    Authentication

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
 app.use('/js', express.static(__dirname + '/node_modules/vue/dist'));
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist'));
+// app.use('/img', express.static(__dirname + '/'))
 
 app.use('/', index);
 
--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,8 @@ var router = express.Router();
 router.get('/', function(req, res) {
     res.render('business/index', { title: 'Tade index page'});
 });
-router.get('/navbar', function(req, res) {
-    res.render('business/navbar');
+router.get('/login', function(req, res) {
+    res.render('business/login');
 });
 
 module.exports = router;
